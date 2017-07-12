2ff58438406b2534760999a0ef14ff69d28148df
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 17:16:09 +0000

    Dev tool initial commit

---
+++ lib/view_tool_devcamp.rb
@@ -0,0 +1,6 @@
+require "view_tool_devcamp/version"
+require "view_tool_devcamp/renderer"
+
+module ViewToolDevcamp
+  # Your code goes here...
+end
---
+++ lib/view_tool_devcamp/version.rb
@@ -0,0 +1,3 @@
+module ViewToolDevcamp
+  VERSION = "0.1.0"
+end
